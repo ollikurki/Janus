bf918cf9ad49b967db51c5e397f49838c66c19fa
ollikurki <57196387+ollikurki@users.noreply.github.com>
2020-09-09 02:43:02 +0300

    Bringin Janus to git

---
+++ app/__init__.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+from flask_bootstrap import Bootstrap
+
+#Defining the variables to use in other sections of the app
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+bootstrap = Bootstrap(app)
+
+
+#Defining an email server blueprints for the purpose of logging, if it will ever be used. SMTP server needed.
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Janus Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+#setting a path for logging info to be maintained locally, doesn't work yet..
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/Janus.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname).: %(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Janus sartup')
+
+from app import routes, models, errors
