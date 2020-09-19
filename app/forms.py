98b13372e6a76170152599f8e5a5e2f127456cf3
ollikurki <57196387+ollikurki@users.noreply.github.com>
2020-09-20 02:34:52 +0300

    V1.1.7
     On branch development
     Changes to be committed:
    	modified:   app/__init__.py
    	modified:   app/forms.py
    	modified:   app/routes.py
    	modified:   app/static/Admin_manual.pdf
    	modified:   app/static/Opettaja_manual.pdf
    	modified:   app/templates/base.html
    	modified:   app/templates/check_attendance.html
    	modified:   app/templates/index.html
    	modified:   app/templates/user_management.html
    	modified:   config.py
    	modified:   version_history.txt

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, SelectMultipleField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, SelectMultipleField, DateField
 from wtforms.validators import DataRequired, ValidationError, EqualTo, Length
 from app.models import User, Person, Student
 from app.models import Groups
 
-
+#Database query to be used by the add_studen() function in routes.py
+#the student group_select() function could use something like this too instead of a global variable
 def groups_query():
     groups_query = Groups.query.all()
     result = [(x.id, x.marking) for x in groups_query]
     return result
 
-#Creating a form for the login function with username and password validated to be required and an submit button.
+#Form for the login() function in routes.py
 class LoginForm(FlaskForm):
     username = StringField('Käyttäjänimi', validators=[DataRequired()])
     password = PasswordField('Salasana', validators=[DataRequired()])
     submit = SubmitField('Kirjaudu sisään')
 
-#Creating a form for the Admin users to add new users to the database.
-#Clearance could be on it's own table to be queried from.
+#Form for the create_user() function in routes.py
 class AdministrationForm(FlaskForm):
     firstname = StringField('Etunimi', validators=[DataRequired(), Length(max=32, message='max 32 merkkiä')])
     lastname = StringField('Sukunimi', validators=[DataRequired(), Length(max=128, message='max 128 merkkiä')])
@@ -35,36 +35,44 @@ class AdministrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Valitse muu käyttäjänimi.')
 
-#Creating the form to add students.
+#Form for the add_student() function in routes.py
 class StudentForm(FlaskForm):
     firstname = StringField('Etunimi', validators=[DataRequired(), Length(max=32, message='max 32 merkkiä')])
     lastname = StringField('Sukunimi', validators=[DataRequired(), Length(max=128, message='max 128 merkkiä')])
     group = SelectField(u'Ryhmä', coerce=int, validators=[DataRequired()])
     submit = SubmitField('Lisää oppilas')
 
-#, choices=[('PTtvtweb3', 'TvTptweb3'), ('testiweb', 'TvTptwebtesti')]
-#Form for attendance logging to select a group which students are queried
+#Form for the group_select() function in routes.py
 class LogAttendanceGroupSelectForm(FlaskForm):
     group_select = SelectField(u'Ryhmä', coerce=int, validators=[DataRequired()])
     submit = SubmitField('Valitse luokkaryhmä')
 
-#Form for attendance checking by which group they belong to
+#Form for the attendance_check() function in routes.py
 class CheckAttendanceGroupSelectForm(FlaskForm):
-    group_select = SelectField(u'Ryhmä', coerce=int, validators=[DataRequired()])
-    by_student = SubmitField(label='Haku oppilaiden nimi järjestyksessä')
-    by_date = SubmitField(label='Haku päivämäärän mukaan laskien')
+    group_select = SelectField(u'Ryhmähaku', coerce=int, validators=[DataRequired()])
+    by_student = SubmitField(label='Nimijärjestyksessä')
+    by_date = SubmitField(label='Päivämäärä laskien')
+    by_name = SubmitField(label='Hae oppilas')
+    student_name = StringField(u'Oppilas', validators=[Length(max=160, message='max 160 merkkiä')])
+    specific_date_start = StringField(u'Alku päivämäärä', validators=[Length(max=12, message='max 12 merkkiä')])
+    specific_date_end = StringField(u'Loppu päivämäärä', validators=[Length(max=12, message='max 12 merkkiä')])
+    by_specific_date = SubmitField(label='Hae päivämäärä')
 
-#Form for logging attendance of students
+#Form for the attendance_selection() function in routes.py
 class AttendanceForm(FlaskForm):
     attendance = SelectMultipleField(u'Oppilaat', coerce=int, validators=[DataRequired()])
     submit = SubmitField(label='Kirjaa läsnäolleeksi')
 
+#Form for add_groups() function in routes.py
 class GroupMarkingForm(FlaskForm):
     marking = StringField('Luokkaryhmä', validators=[DataRequired(), Length(max=16, message='max 16 merkkiä')])
     submit = SubmitField(label='Lisää luokkaryhmä')
 
+#Form for the create_user() and check_user() functions in routes.py
 class GetUserForm(FlaskForm):
-    get_users = SubmitField(label='Hae käyttäjät')
+    get_users = SubmitField(label='Hae kaikki käyttäjät')
+    user_name = StringField(u'Käyttäjä', validators=[Length(max=161, message='max 161 merkkiä')])
+    get_user = SubmitField(label='Hae käyttäjä')
 
 
 
--- config.py
+++ config.py
@@ -1,13 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+#the configuration objects for the software. Could be stated as environment objects in later versions
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:admin@localhost/Janus'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['kurki.olli@outlook.com']
